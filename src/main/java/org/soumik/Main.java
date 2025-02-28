bdca520b07d121ceab4f43be1ef53659144691fb
Soumik1410 <soumik1410code@gmail.com>
2025-02-28 16:26:36 +0530

    Implemented Square Root, Factorial, Natural Log and Power functionality on command line arguments based on command line argument choice

--- src/main/java/org/soumik/Main.java
+++ src/main/java/org/soumik/Main.java
@@ -2,6 +2,28 @@ package org.soumik;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        float a,b;
+	int ch = Integer.parseInt(args[0]);
+	a = Float.parseFloat(args[1]);
+	if(ch == 1)
+	{
+		System.out.println(Math.sqrt(a));
+	}
+	else if(ch == 2)
+	{
+		int result = 1;
+		for (int i = 2; i <= a; i++)
+	            result *= i;
+		System.out.println(result);
+        }
+	else if(ch == 3)
+	{
+		System.out.println(Math.log(a));
+	}
+	else if(ch == 4)
+	{
+		b = Float.parseFloat(args[2]);
+		System.out.println(Math.pow(a,b));
+	}
     }
 }
