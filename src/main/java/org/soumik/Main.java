13b280be4aa8498ef3acb966f6c661aeaf2533ed
Soumik1410 <soumik1410code@gmail.com>
2025-03-01 17:18:21 +0530

    Updated comment in Main.java to trigger webhook delivery

--- src/main/java/org/soumik/Main.java
+++ src/main/java/org/soumik/Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-	/*A comment to test GitSCM webhook delivery and pipeline trigger*/
+	/*A comment to test GITscm webhook delivery and pipeline trigger*/
 	Main calculator = new Main();
 	calculator.input();
     }
